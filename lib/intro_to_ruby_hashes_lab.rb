fcee38c8bac97eecd1f69173865454ff51331884
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-25 13:40:33 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,28 @@
 def new_hash
   # return an empty hash
+  return Hash.new
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  grades = {
+    :name => "John",
+    :age => 32
+  }
+  
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hashTable = {
+    :name => 'Grace Hopper'
+  }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  table2 = {
+    :id => number
+  }
 end
